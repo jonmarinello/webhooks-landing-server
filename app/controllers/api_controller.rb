1b3c98d6cb0774a3603e05b8e632413075bb1d9b
Jon Marinello <jonmarinello@gmail.com>
2017-05-01 16:13:59 -0700

    Changed to render an empty response.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,6 @@
 class ApiController < ApplicationController
   def api_event
     logger.debug "Request received: params: #{params.inspect}, request_body: #{request.body.inspect}"
-    msg = { params: params, request_body: request.body }
-    render :json => msg
+    head :ok, :content_type => 'application/json'
   end
 end
